353c493400499383d5bfc9cd841c52f851e97004
Malykh Oleg <malykholeg@gmail.com>
2011-05-04 18:20:33 +0400

    added examples

---
+++ examples.js
@@ -0,0 +1,15 @@
+
+  // example of the advanced features
+  // full list of params see in source code in simple_paginator.js file
+  var paginator_html = display_paginator({
+    display_next_prev_bittons: 0,
+    display_paginator: 0,
+    pages_count: 1,
+    current_page: 1,
+    display_one_page: 0,
+    shift_page_number_in_url: 0,
+    prev_button_name: '&laquo;',
+    next_button_name: '&raquo;',
+    increase_interval_if_close_to_edge: 2,
+    page_url: '/article.html?p=%page%'});
+
